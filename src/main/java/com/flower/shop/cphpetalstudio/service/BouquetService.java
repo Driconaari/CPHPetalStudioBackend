a4258263b2f503d4ffca3675a75b790419f8ae80
asger <asgervb@gmail.com>
2024-11-21 13:08:47 +0100

    fixed bouqetsservice and SubscriptionService

--- src/main/java/com/flower/shop/cphpetalstudio/service/BouquetService.java
+++ src/main/java/com/flower/shop/cphpetalstudio/service/BouquetService.java
@@ -2,6 +2,7 @@ package com.flower.shop.cphpetalstudio.service;
 
 
 import com.flower.shop.cphpetalstudio.entity.Bouquet;
+import com.flower.shop.cphpetalstudio.repository.BouquetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,17 +12,31 @@ import java.util.List;
 public class BouquetService {
 
     @Autowired
-    private com.flower.shop.cphpetalstudio.repository.BouquetRepository bouquetRepository;
+    private BouquetRepository bouquetRepository;
 
     public List<Bouquet> getAllBouquets() {
         return bouquetRepository.findAll();
     }
 
     public Bouquet getBouquetById(Long id) {
-        return bouquetRepository.findById(id).orElseThrow(() -> new RuntimeException("Bouquet not found"));
+        return bouquetRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Bouquet not found with id: " + id));
     }
 
-    public Bouquet saveBouquet(Bouquet bouquet) {
+    public List<Bouquet> searchBouquets(String name) {
+        return bouquetRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    public Bouquet createBouquet(Bouquet bouquet) {
+        return bouquetRepository.save(bouquet);
+    }
+
+    public Bouquet updateBouquet(Long id, Bouquet bouquetDetails) {
+        Bouquet bouquet = getBouquetById(id);
+        bouquet.setName(bouquetDetails.getName());
+        bouquet.setDescription(bouquetDetails.getDescription());
+        bouquet.setPrice(bouquetDetails.getPrice());
+        bouquet.setImageUrl(bouquetDetails.getImageUrl());
         return bouquetRepository.save(bouquet);
     }
 
--- src/main/java/com/flower/shop/cphpetalstudio/service/SubscriptionService.java
+++ src/main/java/com/flower/shop/cphpetalstudio/service/SubscriptionService.java
@@ -2,6 +2,8 @@ package com.flower.shop.cphpetalstudio.service;
 
 
 import com.flower.shop.cphpetalstudio.entity.Subscription;
+import com.flower.shop.cphpetalstudio.entity.User;
+import com.flower.shop.cphpetalstudio.repository.SubscriptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +14,44 @@ import java.util.List;
 public class SubscriptionService {
 
     @Autowired
-    private com.flower.shop.cphpetalstudio.repository.SubscriptionRepository subscriptionRepository;
+    private SubscriptionRepository subscriptionRepository;
 
-    public List<Subscription> getAllActiveSubscriptions() {
-        return subscriptionRepository.findByEndDateAfter(LocalDate.now());
+    public List<Subscription> getAllSubscriptions() {
+        return subscriptionRepository.findAll();
     }
 
     public Subscription getSubscriptionById(Long id) {
-        return subscriptionRepository.findById(id).orElseThrow(() -> new RuntimeException("Subscription not found"));
+        return subscriptionRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Subscription not found with id: " + id));
+    }
+
+    public List<Subscription> getSubscriptionsByUser(User user) {
+        return subscriptionRepository.findByUser(user);
+    }
+
+    public List<Subscription> getActiveSubscriptions() {
+        return subscriptionRepository.findByEndDateAfter(LocalDate.now());
+    }
+
+    public List<Subscription> getActiveSubscriptionsByUser(User user) {
+        return subscriptionRepository.findByUserAndEndDateAfter(user, LocalDate.now());
+    }
+
+    public Subscription createSubscription(Subscription subscription) {
+        return subscriptionRepository.save(subscription);
+    }
+
+    public Subscription updateSubscription(Long id, Subscription subscriptionDetails) {
+        Subscription subscription = getSubscriptionById(id);
+        subscription.setUser(subscriptionDetails.getUser());
+        subscription.setBouquet(subscriptionDetails.getBouquet());
+        subscription.setStartDate(subscriptionDetails.getStartDate());
+        subscription.setEndDate(subscriptionDetails.getEndDate());
+        subscription.setFrequency(subscriptionDetails.getFrequency());
+        return subscriptionRepository.save(subscription);
+    }
+
+    public void deleteSubscription(Long id) {
+        subscriptionRepository.deleteById(id);
     }
 }
