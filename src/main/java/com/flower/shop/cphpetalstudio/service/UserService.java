6da1562a204e71c6e9c7def62bf7c9ecfb44ba41
asger <asgervb@gmail.com>
2024-11-21 12:49:54 +0100

    added htmls and put them in folders

--- src/main/java/com/flower/shop/cphpetalstudio/service/UserService.java
+++ src/main/java/com/flower/shop/cphpetalstudio/service/UserService.java
@@ -1,8 +1,7 @@
 package com.flower.shop.cphpetalstudio.service;
 
-
-import com.example.flowershop.model.User;
-import com.example.flowershop.repository.UserRepository;
+import com.flower.shop.cphpetalstudio.entity.User;
+import com.flower.shop.cphpetalstudio.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,3 +19,15 @@ public class UserService {
         user.setRole("ROLE_USER");
         return userRepository.save(user);
     }
+
+    public User createAdminUser(String username, String password, String email) {
+        User adminUser = new User();
+        adminUser.setUsername(username);
+        adminUser.setPassword(passwordEncoder.encode(password));
+        adminUser.setEmail(email);
+        adminUser.setRole("ROLE_ADMIN");
+        return userRepository.save(adminUser);
+    }
+
+    // Add other user-related methods
+}
