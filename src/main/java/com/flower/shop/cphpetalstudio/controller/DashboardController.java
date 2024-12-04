529af5c991b59feb050204ea7cafaf69013af64a
asger <asgervb@gmail.com>
2024-12-04 12:09:36 +0100

    changed the DashboardController

--- src/main/java/com/flower/shop/cphpetalstudio/controller/DashboardController.java
+++ src/main/java/com/flower/shop/cphpetalstudio/controller/DashboardController.java
@@ -1,6 +1,8 @@
 package com.flower.shop.cphpetalstudio.controller;
 
+import com.flower.shop.cphpetalstudio.entity.Bouquet;
 import com.flower.shop.cphpetalstudio.entity.User;
+import com.flower.shop.cphpetalstudio.service.BouquetService;
 import com.flower.shop.cphpetalstudio.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -9,21 +11,51 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class DashboardController {
 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private BouquetService bouquetService;
+
     @GetMapping("/dashboard")
     public String dashboard(Model model, Authentication authentication) {
         User user = userService.findByUsername(authentication.getName());
         model.addAttribute("user", user);
+        String username = authentication.getName();
+        List<Bouquet> featuredBouquets = bouquetService.getFeaturedBouquets();
+
+        model.addAttribute("user", user);
+        model.addAttribute("bouquets", featuredBouquets);
 
         if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
             return "admin-dashboard";
         } else {
             return "user-dashboard";
         }
+    }
+
+    @GetMapping("/shop")
+    public String shop() {
+        return "shop";
+    }
+
+    @GetMapping("/cart")
+    public String cart() {
+        return "cart";
+    }
+
+    @GetMapping("/account/edit")
+    public String editProfile() {
+        return "edit-profile";
+    }
+
+    @GetMapping("/account/change-password")
+    public String changePassword() {
+        return "change-password";
     }
 }
