c25e49c1380a79ac9e48bae1e0caba68ccec6839
asger <asgervb@gmail.com>
2024-12-09 19:27:16 +0100

    hese DTOs keep the controller logic clean by abstracting the data into simple objects.

--- src/main/java/com/flower/shop/cphpetalstudio/dto/AddToCartRequest.java
+++ src/main/java/com/flower/shop/cphpetalstudio/dto/AddToCartRequest.java
@@ -1,4 +1,11 @@
 package com.flower.shop.cphpetalstudio.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class AddToCartRequest {
+    private Long bouquetId; // The ID of the bouquet to add
+    private int quantity;   // The quantity to add
 }
--- src/main/java/com/flower/shop/cphpetalstudio/dto/RemoveFromCartRequest.java
+++ src/main/java/com/flower/shop/cphpetalstudio/dto/RemoveFromCartRequest.java
@@ -1,4 +1,10 @@
 package com.flower.shop.cphpetalstudio.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class RemoveFromCartRequest {
+    private Long bouquetId; // The ID of the bouquet to remove
 }
