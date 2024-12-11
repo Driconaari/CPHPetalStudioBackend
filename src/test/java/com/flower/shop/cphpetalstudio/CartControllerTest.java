b822b3f0fe9edd66ae4aef9a47e6ad51e32c4ba6
asger <asgervb@gmail.com>
2024-12-12 00:56:51 +0100

    added test

--- src/test/java/com/flower/shop/cphpetalstudio/CartControllerTest.java
+++ src/test/java/com/flower/shop/cphpetalstudio/CartControllerTest.java
@@ -1,9 +1,11 @@
 package com.flower.shop.cphpetalstudio;// CartControllerTest.java
 
+
 import com.flower.shop.cphpetalstudio.controller.CartController;
 import com.flower.shop.cphpetalstudio.dto.AddToCartRequest;
 import com.flower.shop.cphpetalstudio.dto.RemoveFromCartRequest;
 import com.flower.shop.cphpetalstudio.entity.*;
+import com.flower.shop.cphpetalstudio.repository.CartItemRepository;
 import com.flower.shop.cphpetalstudio.service.BouquetService;
 import com.flower.shop.cphpetalstudio.service.CartService;
 import com.flower.shop.cphpetalstudio.service.UserService;
@@ -32,6 +34,9 @@ class CartControllerTest {
     private BouquetService bouquetService;
 
     @Mock
+    private CartItemRepository cartItemRepository;
+
+    @Mock
     private Authentication authentication;
 
     @InjectMocks
@@ -54,15 +59,23 @@ class CartControllerTest {
         Bouquet bouquet = new Bouquet();
         bouquet.setId(1L);
 
+        Cart cart = new Cart(user);
+        CartItem cartItem = new CartItem();
+        cartItem.setUser(user);
+        cartItem.setBouquet(bouquet);
+        cartItem.setQuantity(2);
+        cartItem.setCart(cart);
+
         when(authentication.getName()).thenReturn("testUser");
         when(userService.findByUsername("testUser")).thenReturn(user);
         when(bouquetService.getBouquetById(1L)).thenReturn(bouquet);
+        when(cartItemRepository.findByUserAndBouquet(user, bouquet)).thenReturn(Optional.of(cartItem));
 
         ResponseEntity<CartItem> response = cartController.addToCart(request, authentication);
 
         assertNotNull(response);
         assertEquals(200, response.getStatusCodeValue());
-        verify(cartService, times(1)).addToCart(user, bouquet, 2);
+        verify(cartItemRepository, times(1)).save(any(CartItem.class));
     }
 
     @Test
