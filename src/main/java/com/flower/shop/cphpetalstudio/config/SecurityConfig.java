d517e41c57476f1c835050f34aeb8abe0c6386f0
asger <asgervb@gmail.com>
2024-12-11 12:48:51 +0100

    removed configuration.setAllowCredentials(true); cause there were two

--- src/main/java/com/flower/shop/cphpetalstudio/config/SecurityConfig.java
+++ src/main/java/com/flower/shop/cphpetalstudio/config/SecurityConfig.java
@@ -67,7 +67,6 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/api/**", configuration);
         source.registerCorsConfiguration("/fonts/**", configuration);
         source.registerCorsConfiguration("/assets/webfonts/**", configuration);
-        configuration.setAllowCredentials(true);
         return source;
     }
 
