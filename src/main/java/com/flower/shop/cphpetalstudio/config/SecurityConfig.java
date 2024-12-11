a858f7420f850ffa8399cdbc0ef4e5db09d0adc9
asger <asgervb@gmail.com>
2024-12-11 20:25:47 +0100

    role user allowed to add to cart

--- src/main/java/com/flower/shop/cphpetalstudio/config/SecurityConfig.java
+++ src/main/java/com/flower/shop/cphpetalstudio/config/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
 
                         // Authenticated-Only Endpoints
                         .requestMatchers("/dashboard").authenticated()
+                        .requestMatchers("/api/cart/add").hasAuthority("ROLE_USER") // Allow users with ROLE_USER to add items to cart
 
                         // Catch-All for All Other Requests
                         .anyRequest().authenticated()
